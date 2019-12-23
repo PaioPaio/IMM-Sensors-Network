242294567c9e1f9ee8c6f0d79de50d3956867043
Lorenzo <lorecioto@gmail.com>
2019-12-23 18:16:35 +0100

    Cambiato Implementazione della transizione

--- Functions/markchange.m
+++ Functions/markchange.m
@@ -4,7 +4,7 @@
 %prende T, fa la cumulata della riga dello stato a cui sono, guarda se un
 %numero preso a caso fra 0 e 1 è minore della cumulata e se lo è va a
 %prendere la colonna risultante -> quello sarà il nostro nuovo stato
-function s1=markchainez(s,T)
+function s1=markchange(s,T)
     statedistr=T(s,:);
     cumulative=cumsum(statedistr);
     statetrans=cumulative>=rand();
--- Functions/markovmove.m
+++ Functions/markovmove.m
@@ -1,39 +1,91 @@
-%% output: (x-> position + speed; a->acceleration)->c; s1->new state
-%% input:  y-> position + speed; y1->acceleration magnitude; s-> state of markov chain;
+%% output: (x-> position + speed)->c; s->next markov state;
+%% input:  y-> position + speed + ; a->acceleration magnitude; s-> state of markov chain, vel->magnitude of constant inizial speed;
 %%         T-> transition Matrix 
 
-function [c, s1]=markovmove(y,y1,s,T,delta)
-    A=[1 0 delta 0;
-           0 1 0 delta;
-           0 0 1 0
-           0 0 0 1];           %state matrix
-    B=[delta^2/2 0;
-            0 delta^2/2
-            delta 0
-            0 delta];          %input matrix (for acceleration
-    s1=markchainez(s,T);
-        switch s1
+function [c, s]=markovmove(y,a,vel,s,T,delta)
+    A=[1 0 delta 0 delta^2/2 0;
+           0 1 0 delta 0 delta^2/2;
+           0 0 1 0 delta 0;
+           0 0 0 1 0 delta;
+           0 0 0 0 0 0;
+           0 0 0 0 0 0];           %state matrix
+        switch s
             case 1
-                %constant speed, no change of direction
-                x=A*y(1:4);
-                a=[0;0];
+                %constant speed, north direction
+                y=[y(1:2);0;vel;0;0];      %if we move in constantspeed we conserve the position but we reset the speed
+                B=[0 0 0 0 0 0]';          %input matrix for no acceleration
+                
             case 2
-                %constant acceleration, no change of direction
-                a=(y1/sqrt(y(1)^2+y(2)^2))*[y(1);y(2)];    %defining acceleration with same direction
-                x=A*y(1:4)+B*a;
+                %constant speed, n-e direction
+                y=[y(1:2);vel/sqrt(2);vel/sqrt(2);0;0];
+                B=[0 0 0 0 0 0]';
+                
             case 3
-                %change of direction through acceleration
-                ct=2*rand()-1;   %cosine of theta, used to determine in which way the guy turns 
-                st=randbool()*sqrt(1-ct^2);
-                a=y1*[ct; st];
-                x=A*y(1:4)+B*a;
+                %constant speed, east direction
+                y=[y(1:2);vel;0;0;0];
+                B=[0 0 0 0 0 0]';
+                
             case 4
-                %change of direction just to make things chaotic
-                ct=2*rand()-1;   %cosine of theta, used to determine in which way the guy turns 
-                st=randbool()*sqrt(1-ct^2);
-                a=[0;0];
-                x=y(1:4);
-                x(3:4)=sqrt(x(3)^2+x(4)^2)*[ct;st];
+                %constant speed, s-e direction
+                y=[y(1:2);vel/sqrt(2);-vel/sqrt(2);0;0];
+                B=[0 0 0 0 0 0]';   
+                
+            case 5
+                %constant speed, south direction
+                y=[y(1:2);0;-vel;0;0];
+                B=[0 0 0 0 0 0]';  
+                
+            case 6
+                %constant speed, s-w direction
+                y=[y(1:2);-vel/sqrt(2);-vel/sqrt(2);0;0];
+                B=[0 0 0 0 0 0]';  
+                
+            case 7
+                %constant speed, west direction
+                y=[y(1:2);-vel;0;0;0];
+                B=[0 0 0 0 0 0]';                
+                
+            case 8
+                %constant speed, n-w direction
+                y=[y(1:2);-vel/sqrt(2);vel/sqrt(2);0;0];
+                B=[0 0 0 0 0 0]';                
+                
+            case 9
+                %constant acceleration, north direction
+                %here we have no need of resetting the speed, we just need
+                %to impose the direction of the acceleration
+                B=[0 0 0 0 0 1]';
+                
+            case 10
+                %constant acceleration, n-e direction
+                B=[0 0 0 0 1 1]'./sqrt(2);
+                
+            case 11
+                %constant acceleration, east direction
+                B=[0 0 0 0 1 0]';
+                
+            case 12
+                %constant acceleration, s-e direction
+                B=[0 0 0 0 1 -1]'./sqrt(2);
+                
+            case 13
+                %constant acceleration, south direction
+                B=[0 0 0 0 0 -1]';
+                
+            case 14
+                %constant acceleration, s-w direction
+                B=[0 0 0 0 -1 -1]'./sqrt(2);
+                
+            case 15
+                %constant acceleration, west direction
+                B=[0 0 0 0 -1 0]';
+                
+            case 16
+                %constant acceleration, n-w direction
+                B=[0 0 0 0 -1 1]'./sqrt(2);
+                
         end
-        c=[x;a];
+        x=A*y+B*a;
+        s=markchange(s,T);
+        c=x;
 end
