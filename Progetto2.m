b11c93980b7300b53b3addb02a9f4181639f7b4b
Lorenzo <lorecioto@gmail.com>
2020-02-01 21:18:33 +0100

    Giusto per

--- Progetto2.m
+++ Progetto2.m
@@ -5,11 +5,11 @@ clc; clear all; close all;
 %1-> markov chain constant speed or costant acceleration
 %2-> unicycle
 
-caso=1;
+caso=2;
 %delta time
 delta=0.05;
 %at which rate do we do the consensus ?
-rate=2;
+rate=20;
 %Maximum number of iteration
 nstop=1000;
 %iterations montecarlo
@@ -116,7 +116,7 @@ s=1;
 
 %Senosors
 range=10;
-R=diag([0.01,(2*pi/360)^2]);               %sensor covariance
+R=diag([0.1,(2*pi/180)^2]);               %sensor covariance
 
 rmserr=[];
 maxerr=[];
@@ -488,7 +488,7 @@ for y=1:nmonte
             
             addpoints(curve1,stato(1,n),stato(2,n))
             drawnow
-            if plotallsenson==1g
+            if plotallsenson==1
                 if lon>=1
                     addpoints(curvesens1,xconsallsensor{n,1}(1),xconsallsensor{n,1}(2));
                     drawnow
